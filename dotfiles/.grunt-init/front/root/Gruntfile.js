44b67dbe26447a078aedfb1429739208ee6a5307
Outsider <outsideris@gmail.com>
2013-12-13 17:42:27 +0900

    set my default jshint rules up

--- dotfiles/.grunt-init/front/root/Gruntfile.js
+++ dotfiles/.grunt-init/front/root/Gruntfile.js
@@ -25,8 +25,8 @@ module.exports = function(grunt) {
   });
 
   // These plugins provide necessary tasks.
-  for (var key in grunt.file.readJSON("package.json").devDependencies) {
-    if (key !== "grunt" && key.indexOf("grunt") === 0) { grunt.loadNpmTasks(key); }
+  for (var key in grunt.file.readJSON('package.json').devDependencies) {
+    if (key !== 'grunt' && key.indexOf('grunt') === 0) { grunt.loadNpmTasks(key); }
   }
 
   // Default task.
