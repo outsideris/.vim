37c504120fe7988d63ee5f217d28d1c73cf11dc8
Outsider <outsideris@gmail.com>
2022-06-14 21:59:11 +0900

    add a script to bootstrap
    Signed-off-by: Outsider <outsideris@gmail.com>

---
+++ dotfiles/bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+# utsider@Edward-MacStudio dotfiles % cp .direnvrc ~/
+# outsider@Edward-MacStudio dotfiles % cp -R .fzf ~/
+# outsider@Edward-MacStudio dotfiles % cp -R .git_template ~/
+# outsider@Edward-MacStudio dotfiles % cp -R .gitconfig ~/
+# outsider@Edward-MacStudio dotfiles % cp -R .gitconfig-work ~/
+# outsider@Edward-MacStudio dotfiles % cp -R .gitignore_global ~/
+# outsider@Edward-MacStudio dotfiles % cp -R .npmrc ~/
+# outsider@Edward-MacStudio dotfiles % cp -R .p10k.zsh ~/
+# outsider@Edward-MacStudio dotfiles % cp -R .tmux.conf ~/
+# outsider@Edward-MacStudio dotfiles % cp -R .vimrc ~/
+# outsider@Edward-MacStudio dotfiles % cp -R .wakatime.cfg ~/
+# outsider@Edward-MacStudio dotfiles % cp -R .zsh ~/
+# outsider@Edward-MacStudio dotfiles % cp -R .zshrc ~/
+# outsider@Edward-MacStudio dotfiles % cp -R bin ~/
+
+cp -R $(<list.txt) ~/temp/shell/
